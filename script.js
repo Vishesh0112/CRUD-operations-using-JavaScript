c2b4353c0879717135972aa18446516655d354b8
Vishesh0112 <64556644+Vishesh0112@users.noreply.github.com>
2020-10-18 23:51:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["collegeID"] = document.getElementById("collegeID").value;
+    formData["courseName"] = document.getElementById("courseName").value;
+    formData["campus"] = document.getElementById("campus").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.collegeID;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.courseName;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.campus;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("collegeID").value = "";
+    document.getElementById("courseName").value = "";
+    document.getElementById("campus").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("collegeIDemp").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("courseName").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("campus").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.collegeID;
+    selectedRow.cells[2].innerHTML = formData.courseName;
+    selectedRow.cells[3].innerHTML = formData.campus;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    //isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
